9ee3b06b04d948aea90efc58a98e67be550758d8
Luigi Buono <luigibuono1611@gmail.com>
2024-04-23 18:26:55 +0200

    Update news.service.ts

--- src/app/news.service.ts
+++ src/app/news.service.ts
@@ -1,26 +1,21 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsService {
-
-  private apiUrl = 'https://newsapi.org/v2/everything?q=tesla&from=2024-03-23&sortBy=publishedAt&apiKey=b759c4e7e9e448fcb92dbcd4bad66e53';
-
-
-  private apiUrl2 = 'https://gnews.io/api/v4/search?q=example&lang=en&country=us&max=10&apikey=acacb96ba43678e76530033b7ebd39e2';
+  private apiUrlSapi = 'https://newsapi.org/v2/everything?q=tesla&from=2024-03-23&sortBy=publishedAt&apiKey=b759c4e7e9e448fcb92dbcd4bad66e53';
+  private apiUrl = 'https://gnews.io/api/v4/search?q=example&lang=en&country=us&max=10&apikey=acacb96ba43678e76530033b7ebd39e2';
 
   constructor(private http: HttpClient) { }
 
   getNews(): Observable<any> {
-    return this.http.get<any>(this.apiUrl);
+    return this.http.get<any>(this.apiUrlSapi);
   }
 
   getGNews(): Observable<any> {
-    return this.http.get<any>(this.apiUrl2);
+    return this.http.get<any>(this.apiUrl);
   }
-
 }

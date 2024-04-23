16df86b848095e16ebe41e105ce74d0d3b31c3d9
Luigi Buono <luigibuono1611@gmail.com>
2024-04-23 18:27:08 +0200

    Update news.service.ts

--- src/app/news.service.ts
+++ src/app/news.service.ts
@@ -6,8 +6,8 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class NewsService {
-  private apiUrlSapi = 'https://newsapi.org/v2/everything?q=tesla&from=2024-03-23&sortBy=publishedAt&apiKey=b759c4e7e9e448fcb92dbcd4bad66e53';
-  private apiUrl = 'https://gnews.io/api/v4/search?q=example&lang=en&country=us&max=10&apikey=acacb96ba43678e76530033b7ebd39e2';
+  private apiUrlSapi = '';
+  private apiUrl = '';
 
   constructor(private http: HttpClient) { }
 
